be76fd50975f61148a5c5d796b882d1b81ccdb9e
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-10-28 21:53:45 +0800

    first commit files

--- promo-scra.py
+++ promo-scra.py
@@ -801,9 +801,9 @@ def job():
 # schedule.every(10).minutes.do(job)
 # schedule.every().hour.do(job)
 # schedule.every().day.at('13:58').do(job)
-schedule.every(5).to(10).minutes.do(job)
+# schedule.every(5).to(10).minutes.do(job)
 # schedule.every().monday.do(job)
-# schedule.every().wednesday.at("13:15").do(job)
+schedule.every().wednesday.at("14:00").do(job)
 # schedule.every().minute.at(":17").do(job)
 
 while True:
