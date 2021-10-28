4cdc4b561ffff8a6bc4873e98424667048cb7f7a
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-10-28 22:13:12 +0800

    changed time for schedule

--- promo-scra.py
+++ promo-scra.py
@@ -803,7 +803,7 @@ def job():
 # schedule.every().day.at('13:58').do(job)
 # schedule.every(5).to(10).minutes.do(job)
 # schedule.every().monday.do(job)
-schedule.every().wednesday.at("14:00").do(job)
+schedule.every().wednesday.at("14:25").do(job)
 # schedule.every().minute.at(":17").do(job)
 
 while True:
