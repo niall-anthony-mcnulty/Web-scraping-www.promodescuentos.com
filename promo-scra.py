5122e5c92a2d44e4de65d5faec8a9813316342d5
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-10-29 01:21:03 +0800

    time

--- promo-scra.py
+++ promo-scra.py
@@ -808,7 +808,7 @@ def job():
 # schedule.every().day.at('13:58').do(job)
 # schedule.every(5).to(10).minutes.do(job)
 # schedule.every().monday.do(job)
-schedule.every().thursday.at("16:47").do(job)
+schedule.every().thursday.at("17:24").do(job)
 # schedule.every().minute.at(":17").do(job)
 
 while True:
